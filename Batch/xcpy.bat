dcca826de7cb34f0c8c4afd2002a43a7cf3bd33e
momusau <mwendwa24@gmail.com>
2017-05-12 18:26:10 -0400

    Cleaning Batch scripts

--- Batch/xcpy.bat
+++ Batch/xcpy.bat
@@ -1,24 +1,22 @@
 #SPECIFIES VARIABLE FOR FOLDERS TO COPY TO
 for %%D in (
 \\sample
-) do xcopy "C:\sample" %%D /v /s /y /z 
+) do xcopy "C:\sample" %%D /v /s /y /z
 
 @echo off
-if errorlevel 4 goto lowmemory 
-if errorlevel 2 goto abort 
-if errorlevel 0 goto exit 
+if errorlevel 4 goto lowmemory
+if errorlevel 2 goto abort
+if errorlevel 0 goto exit
 
-:lowmemory 
-echo Insufficient memory to copy files or 
-echo invalid drive or command-line syntax. 
+:lowmemory
+echo Insufficient memory to copy files or
+echo invalid drive or command-line syntax.
 goto exit
 
-:abort 
-echo You pressed CTRL+C to end the copy operation. 
+:abort
+echo You pressed CTRL+C to end the copy operation.
 goto exit
 
 :exit
 
 exit
-
-
